715167725e7d2c548b9faf4181567016013eecd3
abubakarbeni <abubakarbeni12@gmail.com>
2023-08-19 18:30:46 +0100

    black pieces added

--- board.py
+++ board.py
@@ -24,7 +24,7 @@ class Board(tk.Button):
     
     
     def set_board(self):
-        self.color = "#808080"
+        self.color = "#807c7c"
         for row in range(8):
             
             if self.color == "white":
@@ -50,32 +50,49 @@ class Board(tk.Button):
                 
     def pieces(self):
         
-        global wpawn, wrook, wknight, wbishop, wking, wqueen
+        global wpawn, bpawn, wrook, brook, wknight, bknight, wbishop, bbishop,wking, bking, wqueen, bqueen
         
+        # adding pawns
         wpawn = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/pawn-w.png").subsample(6, 6)
+        bpawn = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/pawn-b.png").subsample(6, 6)
         for i in self.gameboard[6]:
             i.configure(height = 0, width = 0, image = wpawn)
+        for j in self.gameboard[1]:
+            j.configure(height = 0, width = 0, image = bpawn)
         
         # adding rooks
+        brook = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/rook-b.png").subsample(6, 6)
         wrook = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/rook-w.png").subsample(6, 6)
         self.gameboard[7][0].configure(height = 0, width = 0, image = wrook)
         self.gameboard[7][7].configure(height = 0, width = 0, image = wrook)
+        self.gameboard[0][0].configure(height = 0, width = 0, image = brook)
+        self.gameboard[0][7].configure(height = 0, width = 0, image = brook)
         
-        # adding knights
+        # # adding knights
+        bknight = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/knight-b.png").subsample(6, 6)
         wknight = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/knight-w.png").subsample(6, 6)
         self.gameboard[7][6].configure(height = 0, width = 0, image = wknight)
         self.gameboard[7][1].configure(height = 0, width = 0, image = wknight)
+        self.gameboard[0][1].configure(height = 0, width = 0, image = bknight)
+        self.gameboard[0][6].configure(height = 0, width = 0, image = bknight)
         
-        # adding bishops
-        wbishop = tk.PhotoImage(master = root, file = r"/homeuser/chess/pieces/bishop-w.png").subsample(6, 6)
+        # # adding bishops
+        bbishop = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/bishop-b.png").subsample(6, 6)
+        wbishop = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/bishop-w.png").subsample(6, 6)
         self.gameboard[7][2].configure(height = 0, width = 0, image = wbishop)
         self.gameboard[7][5].configure(height = 0, width = 0, image = wbishop)
+        self.gameboard[0][2].configure(height = 0, width = 0, image = bbishop)
+        self.gameboard[0][5].configure(height = 0, width = 0, image = bbishop)
         
-        # adding king and queen
+        # # adding king and queen
+        bking = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/king-b.png").subsample(6, 6)
+        bqueen = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/queen-b.png").subsample(6, 6)
         wking = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/king-w.png").subsample(6, 6)
         wqueen = tk.PhotoImage(master = root, file = r"/home/user/chess/pieces/queen-w.png").subsample(6, 6)
         self.gameboard[7][3].configure(height = 0, width = 0, image = wking)
         self.gameboard[7][4].configure(height = 0, width = 0, image = wqueen)
+        self.gameboard[0][3].configure(height = 0, width = 0, image = bking)
+        self.gameboard[0][4].configure(height = 0, width = 0, image = bqueen)
                 
 board = Board()
 board.set_board()
